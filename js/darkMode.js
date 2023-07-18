fac61bb06f99dd2e554328ad1c960c234f40dfa2
Jonasz Janicki <34692088+PaperManV2@users.noreply.github.com>
2023-07-18 11:30:04 +0200

    little touch of gooood

--- js/darkMode.js
+++ js/darkMode.js
@@ -4,10 +4,23 @@
 
 export function changeOnDarkMode(OnOff, objects) {
     if(OnOff.checked) {
+
+        objects[3].forEach(element => {
+            console.log(element)
+            element.style.backgroundColor = "#7c7c7c"
+        });
+
+        objects[2].style.backgroundColor = "#4e4e4e";
         objects[1].style.boxShadow = "0px 0px 10px 4px #6e6e6ecc";
         objects[0].style.backgroundColor = "#2e2d2d";
- 
     } else {
+
+        objects[3].forEach(element => {
+            console.log(element)
+            element.style.backgroundColor = "#dadada"
+        });
+
+        objects[2].style.backgroundColor = "#dadada";
         objects[1].style.boxShadow = "0px 0px 10px 4px #000000cc";
         objects[0].style.backgroundColor = "#fff";
 
--- js/index.js
+++ js/index.js
@@ -5,7 +5,10 @@ import { changeOnDarkMode } from "./darkMode.js";
 const darkModeSwitch = document.getElementById("darkMode");
 const body = document.querySelector("body");
 const menu = document.getElementById("menu");
-const objects = [body, menu];
+const bigSquare = document.getElementById("bigSquare");
+const littleSquares = document.querySelectorAll(".littleSquare");
+
+const objects = [body, menu, bigSquare, littleSquares];
 
 darkModeSwitch.addEventListener("click", () => {
     changeOnDarkMode(darkModeSwitch, objects);
